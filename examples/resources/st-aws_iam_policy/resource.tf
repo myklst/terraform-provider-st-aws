aabb9220a4ae6ccca789f0c33eff9043b953c5f7
xiaotongwong <wongxiaotong1@gmail.com>
2023-06-29 10:27:49 +0800

    Update argument name in resource.tf

--- examples/resources/st-aws_iam_policy/resource.tf
+++ examples/resources/st-aws_iam_policy/resource.tf
@@ -1,5 +1,5 @@
 resource "st-aws_iam_policy" "iam_policy" {
-  policy_name     = "test-policy"
-  attached_policy = ["PowerUserAccess", "AWSSupportAccess", "AWSLambdaRole", "AmazonSNSRole"]
-  user_name       = "devopsuser01"
+  policy_name       = "test-policy"
+  attached_policies = ["PowerUserAccess", "AWSSupportAccess", "AWSLambdaRole", "AmazonSNSRole"]
+  user_name         = "devopsuser01"
 }
