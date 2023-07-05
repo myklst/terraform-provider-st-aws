4eac3cefd5728ed02bf16073a4af2a65de3aa313
xiaotongwong <wongxiaotong1@gmail.com>
2023-07-05 12:15:20 +0800

    Update ImportState func.

--- aws/resource_iam_policy.go
+++ aws/resource_iam_policy.go
@@ -264,14 +264,92 @@ func (r *iamPolicyResource) Delete(ctx context.Context, req resource.DeleteReque
 }
 
 func (r *iamPolicyResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+	policyDetailsState := []*policyDetail{}
+	getPolicyDocumentResponse := &awsIamClient.GetPolicyVersionOutput{}
+	policyNames := strings.Split(req.ID, ",")
+	var username string
+
+	var err error
+	getPolicy := func() error {
+		for _, policyName := range policyNames {
+			policyName = strings.ReplaceAll(policyName, " ", "")
+
+			// Retrieves the policy document for the policy
+			policyArn, policyVersionId := r.getPolicyArn(ctx, policyName)
+
+			getPolicyDocumentResponse, err = r.client.GetPolicyVersion(ctx, &awsIamClient.GetPolicyVersionInput{
+				PolicyArn: aws.String(policyArn),
+				VersionId: aws.String(policyVersionId),
+			})
+			if err != nil {
+				handleAPIError(err)
+			}
+
+			// Retrieves the name of the user attached to the policy.
+			getPolicyEntities, err := r.client.ListEntitiesForPolicy(ctx, &awsIamClient.ListEntitiesForPolicyInput{
+				PolicyArn: aws.String(policyArn),
+			})
+			if err != nil {
+				handleAPIError(err)
+			}
+
+			if getPolicyDocumentResponse.PolicyVersion != nil {
+				policyDocument, err := url.QueryUnescape(*getPolicyDocumentResponse.PolicyVersion.Document)
+				if err != nil {
+					resp.Diagnostics.AddError(
+						"[API ERROR] Failed to Convert the Policy Document.",
+						err.Error(),
+					)
+				}
+
+				policyDetail := policyDetail{
+					PolicyName:     types.StringValue(policyName),
+					PolicyDocument: types.StringValue(policyDocument),
+				}
+				policyDetailsState = append(policyDetailsState, &policyDetail)
+			}
+
+			if getPolicyEntities.PolicyUsers != nil {
+				for _, user := range getPolicyEntities.PolicyUsers {
+					username = *user.UserName
+				}
+			}
+		}
+		return nil
+	}
+
+	reconnectBackoff := backoff.NewExponentialBackOff()
+	reconnectBackoff.MaxElapsedTime = 30 * time.Second
+	err = backoff.Retry(getPolicy, reconnectBackoff)
+	if err != nil {
+		return
+	}
+
+	var policyList []policyDetail
+	for _, policy := range policyDetailsState {
+		policies := policyDetail{
+			PolicyName:     types.StringValue(policy.PolicyName.ValueString()),
+			PolicyDocument: types.StringValue(policy.PolicyDocument.ValueString()),
+		}
+
+		policyList = append(policyList, policies)
+	}
+
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("user_name"), username)...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("policies"), policyList)...)
+
+	if !resp.Diagnostics.HasError() {
+		resp.Diagnostics.AddWarning(
+			"Unable to Set the attached_policies Attribute",
+			"After running terraform import, Terraform will not automatically set the attached_policies attributes."+
+				"To ensure that all attributes defined in the Terraform configuration are set, you need to run terraform apply."+
+				"This command will apply the changes and set the desired attributes according to your configuration.",
+		)
+	}
 }
 
 func (r *iamPolicyResource) createPolicy(ctx context.Context, plan *iamPolicyResourceModel) (policiesList []attr.Value, err error) {
-	formattedPolicy, err := r.getPolicyDocument(ctx, plan)
-	if err != nil {
-		return nil, err
-	}
+	formattedPolicy, _ := r.getPolicyDocument(ctx, plan)
 
 	createPolicy := func() error {
 		for i, policy := range formattedPolicy {
@@ -430,11 +508,11 @@ func (r *iamPolicyResource) removePolicy(ctx context.Context, state *iamPolicyRe
 	return nil
 }
 
-func (r *iamPolicyResource) getPolicyDocument(ctx context.Context, plan *iamPolicyResourceModel) (finalPolicyDocument []string, err error) {
+func (r *iamPolicyResource) getPolicyDocument(ctx context.Context, plan *iamPolicyResourceModel) ([]string, diag.Diagnostics) {
 	currentLength := 0
 	currentPolicyDocument := ""
 	appendedPolicyDocument := make([]string, 0)
-	finalPolicyDocument = make([]string, 0)
+	finalPolicyDocument := make([]string, 0)
 
 	var getPolicyResponse *awsIamClient.GetPolicyVersionOutput
 
@@ -460,18 +538,33 @@ func (r *iamPolicyResource) getPolicyDocument(ctx context.Context, plan *iamPoli
 
 		tempPolicyDocument, err := url.QueryUnescape(*getPolicyResponse.PolicyVersion.Document)
 		if err != nil {
-			return nil, err
+			return nil, diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"[API ERROR] Failed to Retrieve the Policy Document.",
+					err.Error(),
+				),
+			}
 		}
 
 		var data map[string]interface{}
 		if err := json.Unmarshal([]byte(tempPolicyDocument), &data); err != nil {
-			return nil, err
+			return nil, diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"[API ERROR] Failed to Convert the Policy Document.",
+					err.Error(),
+				),
+			}
 		}
 
 		statementArr := data["Statement"].([]interface{})
 		statementBytes, err := json.MarshalIndent(statementArr, "", "  ")
 		if err != nil {
-			return nil, err
+			return nil, diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"[API ERROR] Failed to Convert the Policy Document.",
+					err.Error(),
+				),
+			}
 		}
 
 		removeSpaces := strings.ReplaceAll(string(statementBytes), " ", "")
